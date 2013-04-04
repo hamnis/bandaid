2530b2ef781d1e328dd235e78c5d099c647f199d
Erlend Hamnaberg <erlend@hamnaberg.net>
2013-04-04 21:11:35 +0200

    @tailrec added

--- src/main/scala/pointy/Pointer.scala
+++ src/main/scala/pointy/Pointer.scala
@@ -1,4 +1,5 @@
 import org.json4s.JsonAST._
+import annotation.tailrec
 
 /**
  Implementation of JSON Pointer. RFC-6901
@@ -13,7 +14,7 @@ object Pointer {
     if (selected.trim.isEmpty) Nil
     else {
       val parts = ( if (selected.startsWith("/")) selected.substring(1) else selected).split("/").toList.map(unescape)
-      def req(parts: List[String], acc:List[Ref]): List[Ref] = {
+      @tailrec def req(parts: List[String], acc:List[Ref]): List[Ref] = {
         parts match {
           case Nil => acc
           case AsInt(x) :: xs => req(xs, acc ++ List(ArrayRef(x)))
@@ -25,13 +26,14 @@ object Pointer {
   }
   
   private def select(parts: List[Ref], doc: JValue): JValue = {
-    def req(parts: List[Ref], in: JValue): JValue = {
+    @tailrec def req(parts: List[Ref], in: JValue): JValue = {
       parts match {
         case Nil => in
         case ArrayRef(x) :: xs => in match {
           case JArray(list) if (list.isDefinedAt(x)) => req(xs, list(x))
           case _ => JNothing
         }
+        case EndOfArray :: _ => JNothing
         case PropertyRef(name) :: xs => in match {
           case JObject(list) => req(xs, list.find{case JField(n, v) => n == name}.map(_._2).getOrElse(JNothing))
           case _ => JNothing
@@ -53,5 +55,6 @@ sealed trait Ref
 
 object Ref {
   case class ArrayRef(index:Int) extends Ref
+  case object EndOfArray extends Ref
   case class PropertyRef(name:String) extends Ref
 }
