732a3e592ff5d086867eb48c12c8a328d7039435
Erlend Hamnaberg <erlend@hamnaberg.net>
2013-07-25 21:25:15 +0200

    Typeclass FTW :)

--- src/main/scala/pointy/Pointer.scala
+++ src/main/scala/pointy/Pointer.scala
@@ -19,22 +19,14 @@ package pointy
 import org.json4s.JsonAST._
 import annotation.tailrec
 
-trait JsonPointer {
-  def document: JValue
-  def select(selector: Selector): Option[JValue]
-  def update(selector: Selector, update: JValue): JValue
-  def add(selector: Selector, newValue: JValue): JValue
-  def remove(selector: Selector): JValue
-}
-
 /**
  Implementation of JSON Pointer. RFC-6901
  https://tools.ietf.org/html/rfc6901
 */
-case class Pointer(document: JValue) extends JsonPointer {
-  import Selector._
+case class Pointer(document: JValue) {
+  import Ref._
 
-  def select(selector: Selector): Option[JValue] = {
+  def select[A](selector: A)(implicit s: Selector[A]): Option[JValue] = {
     @tailrec def recur(parts: List[Ref], in: JValue): Option[JValue] = {
       parts match {
         case Nil => in match {
@@ -52,10 +44,10 @@ case class Pointer(document: JValue) extends JsonPointer {
         }
       }
     }
-    recur(selector.refs, document)
+    recur(s(selector), document)
   }
 
-  def update(selector: Selector, replacement: JValue): JValue = {
+  def update[A](selector: A, replacement: JValue)(implicit s: Selector[A]): JValue = {
     def recur(s: List[Ref], in: JValue): JValue = {
       s match {
         case Nil => in
@@ -77,10 +69,10 @@ case class Pointer(document: JValue) extends JsonPointer {
         case EndOfArray :: xs => sys.error("List index is out-of-bounds")
       }
     }
-    recur(selector.refs, document)
+    recur(s(selector), document)
   }
 
-  def add(selector: Selector, toAdd: JValue): JValue = {
+  def add[A](selector: A, toAdd: JValue)(implicit s: Selector[A]): JValue = {
     def recur(s: List[Ref], in: JValue): JValue = {
       s match {
         case Nil => in
@@ -103,52 +95,9 @@ case class Pointer(document: JValue) extends JsonPointer {
         }
       }
     }
-    recur(selector.refs, document)
+    recur(s(selector), document)
   }
 
-  def remove(selector: Selector): JValue = update(selector, JNothing)
+  def remove[A](selector: A)(implicit s: Selector[A]): JValue = update(selector, JNothing)
 
-}
-
-case class Path(seg: List[String]) {
-  def toSelector = seg match {
-    case Nil => Selector("")
-    case xs => Selector(xs.mkString("/", "/", ""))
-  }
-}
-
-case class Selector(in: String) {
-
-  private[pointy] val refs: List[Selector.Ref] = {
-    import Selector._
-    if (in.trim.isEmpty) Nil
-    else {
-      val parts = ( if (in.startsWith("/")) in.substring(1) else in).split("/").toList.map(unescape)
-      @tailrec def recur(parts: List[String], acc:List[Ref]): List[Ref] = {
-        parts match {
-          case Nil => acc
-          case AsInt(x) :: xs => recur(xs, acc ++ List(ArrayRef(x)))
-          case x :: xs => recur(xs, acc ++ List(PropertyRef(x)))
-        }
-      }
-      recur(parts, Nil)
-    }
-  }
-  private def unescape(str: String): String = str.replace("~1", "/").replace("~0", "~")
-}
-
-object Selector {
-  implicit def toSelector(s: String) = Selector(s)
-
-  private[pointy] sealed trait Ref
-
-  private[pointy] case class ArrayRef(index:Int) extends Ref
-  private[pointy] case object EndOfArray extends Ref
-  private[pointy] case class PropertyRef(name:String) extends Ref
-
-
-  private[pointy] object AsInt {
-    import util.control.Exception.allCatch
-    def unapply(str: String): Option[Int] = allCatch.opt(str.toInt)
-  }
 }
---
+++ src/main/scala/pointy/Selector.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013 Erlend Hamnaberg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pointy
+
+import scala.annotation.tailrec
+
+trait Selector[A] {
+  def apply(value: A): List[Ref]
+}
+
+object Selector {
+  implicit object StringSelector extends Selector[String] {
+    def apply(value: String): List[Ref] = {
+      import Ref._
+      if (value.trim.isEmpty) Nil
+      else {
+        val parts = ( if (value.startsWith("/")) value.substring(1) else value).split("/").toList.map(unescape)
+        @tailrec def recur(parts: List[String], acc:List[Ref]): List[Ref] = {
+          parts match {
+            case Nil => acc
+            case AsInt(x) :: xs => recur(xs, acc ++ List(ArrayRef(x)))
+            case x :: xs => recur(xs, acc ++ List(PropertyRef(x)))
+          }
+        }
+        recur(parts, Nil)
+      }
+    }
+
+    implicit object PathSelector extends Selector[List[String]] {
+      def apply(value: List[String]) = value match {
+        case Nil => StringSelector("")
+        case xs => StringSelector(xs.mkString("/", "/", ""))
+      }
+    }
+
+    def unescape(str: String): String = str.replace("~1", "/").replace("~0", "~")
+  }
+}
+
+sealed trait Ref
+
+object Ref{
+  case class ArrayRef(index:Int) extends Ref
+  case object EndOfArray extends Ref
+  case class PropertyRef(name:String) extends Ref
+
+  private[pointy] object AsInt {
+    import util.control.Exception.allCatch
+    def unapply(str: String): Option[Int] = allCatch.opt(str.toInt)
+  }
+}
